e53a89e84e336276a2d055919ccd051b7b17443d
Ani <animuthu03@gmail.com>
2024-03-12 10:25:15 +0530

    Feature added

--- Sample.java
+++ Sample.java
@@ -1,11 +1,7 @@
-class Sample
-  {
-    public static void main(String args[])
-    {
-      System.out.println("HI");
-      System.out.println("Good morning");
-      System.out.println("Happy morning");
-      System.out.println("Happy day");
+class main
+{
+	public static void main(String args[])
+	{
+	}
 
-    }
-  }
+}
